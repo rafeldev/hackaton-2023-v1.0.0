04e7fc2fb81d3588206519f63bf7d3c60b89ac41
edgar <a.edgar2209@gmail.com>
2023-01-14 15:25:00 -0500

    create/Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Header } from "../components/Header";
 
 const Home = () => {
-  return <div>Soy el Home - Página de Inicio</div>;
+  return (
+    <div>
+      {/* Soy el Home - Página de Inicio */}
+      <Header />
+    </div>);
 };
 
 export default Home;

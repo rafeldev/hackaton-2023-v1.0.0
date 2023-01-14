ef776abbf9870bc0a8d46563a6ff450b56b5f9d5
pdromoreno7 <pdromorenodev@gmail.com>
2023-01-14 16:43:16 -0500

    add/singing-inpust

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,15 +1,119 @@
-import { Flex, Heading } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import LoginCard from "../../components/components-commos/LoginCard";
 
 function SignIn() {
+  const [show, setShow] = useState(false);
+  const handleShowPassword = () => setShow(!show);
+  const {
+    handleSubmit,
+    control,
+    formState: { errors, isValid },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
       <Flex direction="column" align="center" padding="8rem" height="90vh">
         <LoginCard>
-          <Heading as="h2" size="md" noOfLines={1} color="fontColor">
-            ¿Qué rol tienes?
-          </Heading>
+          <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
+            {/* <Flex direction="column" columnGap={4} p={4}> */}
+            <Controller
+              control={control}
+              name="correo"
+              rules={{
+                required: {
+                  value: true,
+                  message: "El correo es obligatorio",
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "Ingresa un correo válido",
+                },
+              }}
+              render={({
+                field: { value, onChange, onBlur },
+                fieldState: { error },
+              }) => (
+                <Input
+                  size="md"
+                  placeholder="correo"
+                  name="correo"
+                  autoComplete="off"
+                  value={value}
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  isInvalid={error?.ref.correo === "correo"}
+                />
+              )}
+            />
+            {errors.correo && (
+              <Box display="flex" alignItems="center" pb={4}>
+                <Text fontSize="xs" color={"red"}>
+                  {errors.correo.message}
+                </Text>
+              </Box>
+            )}
+            <Controller
+              control={control}
+              name="contraseña"
+              rules={{
+                required: {
+                  value: true,
+                  message: "La contraseña es obligatoria",
+                },
+              }}
+              render={({
+                field: { value, onChange, onBlur },
+                fieldState: { error },
+              }) => (
+                <InputGroup>
+                  <Input
+                    size="md"
+                    type={show ? "text" : "password"}
+                    placeholder="Contraseña"
+                    name="contraseña"
+                    autoComplete="off"
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={onChange}
+                    isInvalid={error?.ref.name === "contraseña"}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handleShowPassword}>
+                      {show ? "Hide" : "Show"}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              )}
+            />
+            {errors.contraseña && (
+              <Box display="flex" alignItems="center" pb={4}>
+                <Text fontSize="xs" color={"red"}>
+                  {errors.contraseña.message}
+                </Text>
+              </Box>
+            )}
+            {/* </Flex> */}
+
+            <Button colorScheme="blue">Enviar</Button>
+          </form>
+
+          {/* <Heading as="h2" size="md" noOfLines={1} color="fontColor">
+            ¿Qué rol tienes? lore
+          </Heading> */}
         </LoginCard>
       </Flex>
     </>

5238eca851aa32466b601360ab91be72790dbe8f
Rafael Alvarez <apiux@MacBook-Pro-de-APIUXNB1203.local>
2023-01-14 16:11:39 -0500

    avanzando en la creacion del register form

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,63 +1,153 @@
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Box, Input, Card, CardBody, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+
 import LoginCard from "../components-commos/LoginCard";
 
 const RegisterForm = () => {
+  const [show, setShow] = React.useState(false);
+  const handleShowPassword = () => setShow(!show);
   const {
     handleSubmit,
     control,
     formState: { errors, isValid },
-  } = useForm();
-
+  } = useForm({ mode: "onChange" });
+  console.log(errors, "errores");
   const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box boxSize="md">
-        <h1>Register</h1>
-        <LoginCard>
-          <Stack spacing={3} p={3}>
-            <Controller
-              control={control}
+    <LoginCard>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="nombre"
+          rules={{
+            required: {
+              value: true,
+              message: "El nombre es obligatorio",
+            },
+            min: {
+              value: 20,
+              message: "Ingresa un número de 9 dígitos",
+            },
+            minLength: {
+              value: 20,
+              message: "Ingresa un número de 9 dígitos",
+            },
+          }}
+          render={({
+            field: { value, onChange, onBlur },
+            fieldState: { error },
+          }) => (
+            <Input
+              size="md"
+              placeholder="Nombre o apodo"
               name="nombre"
-              rules={{
-                required: {
-                  value: true,
-                  message: "El nombre es obligatorio",
-                },
-                min: {
-                  value: 20,
-                  message: "Ingresa un número de 9 dígitos",
-                },
-                minLength: {
-                  value: 20,
-                  message: "Ingresa un número de 9 dígitos",
-                },
-              }}
-              render={({
-                field: { value, onChange, onBlur },
-                fieldState: { error },
-              }) => (
-                <Input
-                  size="md"
-                  placeholder="Nombre o apodo"
-                  name="nombre"
-                  autoComplete="off"
-                  value={value}
-                  onBlur={onBlur}
-                  onChange={onChange}
-                  isInvalid={error?.ref.name === "nombre"}
-                />
-              )}
+              autoComplete="off"
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+              isInvalid={error?.ref.name === "nombre"}
             />
-            <button type="submit">enviar</button>
-          </Stack>
-        </LoginCard>
-      </Box>
-    </form>
+          )}
+        />
+
+        {errors.nombre && (
+          <Box display="flex" alignItems="center" pb={4}>
+            <Text fontSize="xs" color={"red"}>
+              {errors.nombre.message}
+            </Text>
+          </Box>
+        )}
+
+        <Controller
+          control={control}
+          name="correo"
+          rules={{
+            required: {
+              value: true,
+              message: "El correo es obligatorio",
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Ingresa un correo válido",
+            },
+          }}
+          render={({
+            field: { value, onChange, onBlur },
+            fieldState: { error },
+          }) => (
+            <Input
+              size="md"
+              placeholder="Correo"
+              name="correo"
+              autoComplete="off"
+              value={value}
+              onBlur={onBlur}
+              onChange={onChange}
+              isInvalid={error?.ref.name === "correo"}
+            />
+          )}
+        />
+        {errors.correo && (
+          <Box display="flex" alignItems="center" pb={4}>
+            <Text fontSize="xs" color={"red"}>
+              {errors.correo.message}
+            </Text>
+          </Box>
+        )}
+        <Controller
+          control={control}
+          name="contraseña"
+          rules={{
+            required: {
+              value: true,
+              message: "La contraseña es obligatoria",
+            },
+          }}
+          render={({
+            field: { value, onChange, onBlur },
+            fieldState: { error },
+          }) => (
+            <InputGroup>
+              <Input
+                size="md"
+                type={show ? "text" : "password"}
+                placeholder="Contraseña"
+                name="contraseña"
+                autoComplete="off"
+                value={value}
+                onBlur={onBlur}
+                onChange={onChange}
+                isInvalid={error?.ref.name === "contraseña"}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleShowPassword}>
+                  {show ? "Hide" : "Show"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          )}
+        />
+        {errors.contraseña && (
+          <Box display="flex" alignItems="center" pb={4}>
+            <Text fontSize="xs" color={"red"}>
+              {errors.contraseña.message}
+            </Text>
+          </Box>
+        )}
+        <button type="submit">enviar</button>
+      </form>
+    </LoginCard>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "../../components/Header/Header";
+import RegisterForm from "../../components/RegisterForm";
 
 const Home = () => {
   return (
     <div>
       {/* Soy el Home - Página de Inicio */}
       <Header />
+      <RegisterForm />
     </div>
   );
 };

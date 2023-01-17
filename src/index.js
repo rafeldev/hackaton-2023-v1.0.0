708a198cbcf2576c0160dc8fa26dc57f7ec15ed8
edgar <a.edgar2209@gmail.com>
2023-01-17 12:48:05 -0500

    add/agregué provider de redux

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { store } from "./redux/store";
 //chacra-ui
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider >
   </React.StrictMode>
 );

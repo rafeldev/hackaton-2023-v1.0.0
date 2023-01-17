4a887039e35aa51f495fdfbe324bf3d281a87cb2
Eibeel <abelguardop@gmail.com>
2023-01-17 09:40:30 -0500

    updated: Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { Avatar, Flex, Image, Popover, PopoverBody, PopoverContent, PopoverHeader, PopoverTrigger, Text } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import Logo from "../../assets/logo.jpg";
 
@@ -12,13 +12,26 @@ export const Header = () => {
       alignItems="center"
     >
       <Image src={Logo} alt="Logo" />
-      <HStack spacing="1rem">
-        <Avatar size="sm" src="https://bit.ly/sage-adebayo" />
-        <Text fontWeight="normal" color="blackAlpha.700">
+      <Flex columnGap="1rem" alignItems="center">
+        <Avatar name="Rick Adalberto" size="sm" /> {/* name={string} --> iniciales del usuario autenticado */}
+        <Text fontWeight="normal" color="blackAlpha.700" fontSize="18px">
           Rick Adalberto
         </Text>
-        <ChevronDownIcon cursor="pointer" />
-      </HStack>
+        <Popover>
+          <PopoverTrigger>
+            <ChevronDownIcon cursor="pointer" />
+          </PopoverTrigger>
+          <PopoverContent w="fit-content" h="auto" mr="80px" p="1rem 1rem .5rem" boxShadow="0px 6px 36px 0px #0000001A" border="1px" borderColor="gray.200">
+            <PopoverHeader display="flex" alignItems="center" columnGap="1rem" pb="1rem">
+              <Avatar name="Rick Adalberto" w="48px" h="48px" /> {/* name={string} --> iniciales del usuario autenticado */}
+              <Text color="blackAlpha.700" fontSize="16px" fontWeight="500">rick_adalberto@url.short</Text> {/* {string} --> correo del usuario autenticado */}
+            </PopoverHeader>
+            <PopoverBody p="8px 0px">
+              <Text cursor="pointer" fontSize="16px" color="blackAlpha.700" w="fit-content">Cerrar sesión</Text>
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+      </Flex>
     </Flex>
   );
 };

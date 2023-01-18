1778f4230b2bbe31f2ea89bdcc4759f6afd14548
Eibeel <abelguardop@gmail.com>
2023-01-17 20:24:13 -0500

    refactor: Manejo de errores de autenticacion

--- src/services/auth/register.service.js
+++ src/services/auth/register.service.js
@@ -1,31 +1,36 @@
 import { auth } from "../../firebase/config.firebase";
 import {
-    createUserWithEmailAndPassword,
-    updateProfile,
-  } from "firebase/auth";
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 
 export const registerUser = async (correo, contraseña, nombre) => {
-      try {
-        const { user } = await createUserWithEmailAndPassword  (
-          auth,
-          correo,
-          contraseña
-        );
-        
-        //en firebase por defecto sale el nombre de usuario como null, esta linea actualiza 
-        //el nombre de usuario en la base de datos
-         await updateProfile(user, {
-          displayName: nombre,
-        });
+  try {
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      correo,
+      contraseña
+    );
 
-        return {
-            uid: user.uid,
-            userName: user.displayName,
-          };
+    //en firebase por defecto sale el nombre de usuario como null, esta linea actualiza 
+    //el nombre de usuario en la base de datos
+    await updateProfile(user, {
+      displayName: nombre,
+    });
 
-      } catch (error) {
-        console.log(error);
-        throw new Error(error.message)
-      }
+    return {
+      uid: user.uid,
+      userName: user.displayName,
     };
-  
+
+  } catch (error) {
+
+    if (error.message === 'Firebase: Error (auth/email-already-in-use).') {
+      return {
+        ok: false,
+        errorMessage: 'El correo electrónico ya está en uso.'
+      }
+    }
+    console.log(error.message);
+  }
+};
